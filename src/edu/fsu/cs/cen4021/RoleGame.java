5d9edaa2b830e136db600e60715ca4f096daaae1
4thJenkins17 <asa4jenkins@gmail.com>
2017-01-27 15:10:04 -0500

    Updating RoleGame.java so that it displays information about weapons

--- src/edu/fsu/cs/cen4021/RoleGame.java
+++ src/edu/fsu/cs/cen4021/RoleGame.java
@@ -10,14 +10,28 @@ public class RoleGame {
 
     public static void main(String[] args) {
         Weapon sword = WeaponFactory.getWeapon("sword");
-//        Weapon arrow = WeaponFactory.getWeapon("arrow");            // Uncomment to test arrow
-//        Weapon axe = WeaponFactory.getWeapon("axe");                // Uncomment to test axe
-        Weapon staff = WeaponFactory.getWeapon("staff");
-        System.out.println("Sword has " + staff.hit() + " of damage.");
+
+        System.out.println("Sword has " + sword.hit() + " of damage.");
         int armor = 17;
-        System.out.println("Swords were able to do " + staff.hit(armor) + " of damage due to an armor with " + armor + " points.");
+        System.out.println("Swords were able to do " + sword.hit(armor) + " of damage due to an armor with " + armor + " points.");
 
         //TODO: Add the remaining weapons here
+
+        Weapon arrow = WeaponFactory.getWeapon("arrow");
+        Weapon axe = WeaponFactory.getWeapon("axe");
+        Weapon staff = WeaponFactory.getWeapon("staff");
+        
+        System.out.println("Arrow has " + arrow.hit() + " of damage.");
+        armor = 25;
+        System.out.println("Arrows were able to do " + arrow.hit(armor) + " of damage due to an armor with " + armor + " points.");
+
+        System.out.println("Axe has " + axe.hit() + " of damage.");
+        armor = 50;
+        System.out.println("Axes were able to do " + axe.hit(armor) + " of damage due to an armor with " + armor + " points.");
+
+        System.out.println("Staff has " + staff.hit() + " of damage.");
+        armor = 50;
+        System.out.println("Staffs were able to do " + staff.hit(armor) + " of damage due to an armor with " + armor + " points.");
 
     }
 }
